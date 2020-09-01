206d5dade804c1095ad53c3b15ab8b89b5655112
Sckende <clrccljuhasz@gmail.com>
2020-09-01 12:20:43 -0400

    # Obtention du jeu de données d'observations par sites

--- app2.R
+++ app2.R
@@ -7,21 +7,19 @@ library(dplyr)
 library(shinythemes)
 
 
-#if(!exists("all_obs_sp_df")){source("Manipulations_rcoleo.R")}
+if(!exists("all_obs")){source("Manipulations_rcoleo.R")}
+#source("Manipulations_rcoleo.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("slate"),
                 fluidRow(
                   column(4,
-                         # selectInput("site",
-                         #             "Code du site",
-                         #             c("all", j$site_code)
-                         #
-                         #            ),
+                         selectInput("site",
+                                     "Code du site",
+                                     c("all", sort(unique(all_obs$site_code)))),
                          selectInput("hab",
                                      "Type d'habitat",
-                                     #sort(unique(df$hab)),
-                                     c("all", sort(unique(j$type)))
+                                     c("all", sort(unique(all_sites$type)))
                          )
                          
                   ),
@@ -30,42 +28,38 @@ ui <- fluidPage(theme = shinytheme("slate"),
                   
                   
                 ),
-                fluidRow(#dataTableOutput("donnees"))
-                  tableOutput("donnees"))
+                fluidRow(
+                dataTableOutput("donnees"))
 )
 
-# Define server logic required to draw a histogram
+# ---------------------------------------------------- #
+# ---------------------------------------------------- #
+
 server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
     if(input$hab == "all"){
       leaflet() %>%
         addTiles() %>% # Affichage du fond de carte
-        addCircleMarkers(lng = j$long_site, # Positionnement des sites avec les coordonnées long/lat
-                         lat = j$lat_site,
+        addCircleMarkers(lng = all_sites$long_site, # Positionnement des sites avec les coordonnées long/lat
+                         lat = all_sites$lat_site,
                          radius = 8, # taille du cercle
-                         popup = j$popup_info, # Ajout de fenêtres pop-up
-                         color = j$col)
+                         popup = all_sites$popup_info, # Ajout de fenêtres pop-up
+                         color = all_sites$col)
     }else{
       
       leaflet() %>%
         addTiles() %>% # Affichage du fond de carte
-        addCircleMarkers(lng = j$long_site[j$type == input$hab], # Positionnement des sites avec les coordonnées long/lat
-                         lat = j$lat_site[j$type == input$hab],
+        addCircleMarkers(lng = all_sites$long_site[all_sites$type == input$hab], # Positionnement des sites avec les coordonnées long/lat
+                         lat = all_sites$lat_site[all_sites$type == input$hab],
                          radius = 8, # taille du cercle
-                         popup = j$popup_info[j$type == input$hab], # Ajout de fenêtres pop-up
-                         color = unique(j$col[j$type == input$hab]))
+                         popup = all_sites$popup_info[all_sites$type == input$hab], # Ajout de fenêtres pop-up
+                         color = unique(all_sites$col[all_sites$type == input$hab]))
       
     }
   })
-  output$donnees <- renderTable({
-#    if(input$hab == "all"){
-    renderTable(head(all_obs_sp_df))
-#      renderDataTable(all_obs_sp_df, options = list(pageLength = 5))
- #   } else {
-    #   renderTable(all_sp_df[all_obs_sp_df$type == input$hab], options = list(pageLength = 5))
-    # }
-  })
+  output$donnees <- renderDataTable(all_obs[all_obs$site_code == input$site,],                                     options = list(pageLength = 50))
+
 }
 
 # Run the application
