6e227354d65c4680d84526c046bbc9298e9afcee
Sckende <clrccljuhasz@gmail.com>
2020-09-01 15:58:36 -0400

    # Test sur les waffle plots

--- Interactive_maps_tests.R
+++ Interactive_maps_tests.R
@@ -12,7 +12,7 @@ library(leaflet)
 library(dplyr)
 
 source("functions.R")
-source("Manipulation_rcoleo.R")
+source("Manipulations_rcoleo.R")
 
 ### ---------------LEAFLET TESTS ------------------------- ###
 
@@ -25,3 +25,44 @@ leaflet() %>%
                    popup = j$popup_info, # Ajout de fenêtres pop-up
                    color = c("#66CC00", "#000066", "#666600", "#003333", "#0066CC", "#FF9900", "#660000")[as.integer(as.factor(j$type))])
 
+### -------------- INTERACTIVE WAFFLE PLOT TESTS ---------------- ###
+
+# library(waffle)
+# packageVersion("waffle")
+# library(magrittr)
+# library(hrbrthemes)
+# library(ggplot2)
+# library(dplyr)
+# 
+# xdf <- iris %>% count(Species)
+# class(w)
+
+
+storms %>% 
+  filter(year == 2010) %>% 
+  count(status)
+
+all_obs %>%  filter(site_code == "142_111_F01") %>% count(type) -> xdf
+
+library(hrbrthemes)
+library(waffle)
+library(tidyverse)
+
+# tibble(
+#   parts = factor(rep(month.abb[1:3], 3), levels=month.abb[1:3]),
+#   values = c(10, 20, 30, 6, 14, 40, 30, 20, 10),
+#   fct = c(rep("Thing 1", 3), rep("Thing 2", 3), rep("Thing 3", 3))
+# ) -> xdf
+
+ggplot(xdf, aes(fill=type, values=n)) +
+  geom_waffle(color = "white", size=1.125, n_rows = 6) +
+  #facet_wrap(~fct, ncol=1) +
+ # scale_x_discrete(expand=c(0,0)) +
+ # scale_y_discrete(expand=c(0,0)) +
+ # ggthemes::scale_fill_tableau(name=NULL) +
+  coord_equal() +
+  labs(
+    title = "Faceted Waffle Geoms"
+  ) +
+  theme_ipsum_rc(grid="") +
+  theme_enhance_waffle()
