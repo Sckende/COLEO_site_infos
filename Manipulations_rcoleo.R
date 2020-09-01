374ffe99c208e98152a2176ef4f4a9942848f935
Sckende <clrccljuhasz@gmail.com>
2020-09-01 12:20:16 -0400

    # Changement du nom du DF avec tous les sites # DF avec toutes les observations

--- Manipulations_rcoleo.R
+++ Manipulations_rcoleo.R
@@ -5,29 +5,29 @@ library(rcoleo)
 library(dplyr)
 library(purrr)
 
-#source("functions.R")
+source("functions.R")
 
 # Obtention des informations pour tous les sites
-j <- get_sites()[[1]][[1]][[1]]
-#utils::View(j)
-names(j)
+all_sites <- get_sites()[[1]][[1]][[1]]
+#utils::View(all_sites)
+names(all_sites)
 
 # Obtention des coordonnées des sites d'échantillonnage
-#utils::View(j %>% select(
+#utils::View(all_sites %>% select(
 #  geom.coordinates))
-j$geom.coordinates[[1]]
-k <- as.data.frame(do.call("rbind", j$geom.coordinates))
+all_sites$geom.coordinates[[1]]
+k <- as.data.frame(do.call("rbind", all_sites$geom.coordinates))
 k
-j <- j %>% 
-  mutate(long_site = do.call("rbind", j$geom.coordinates)[,1],
-         lat_site = do.call("rbind", j$geom.coordinates)[,2])
-names(j)
+all_sites <- all_sites %>% 
+  mutate(long_site = do.call("rbind", all_sites$geom.coordinates)[,1],
+         lat_site = do.call("rbind", all_sites$geom.coordinates)[,2])
+names(all_sites)
 
 # Association d'une couleur par habitat
-j$col <- c("#66CC00", "#000066", "#666600", "#003333", "#0066CC", "#FF9900", "#660000")[as.integer(as.factor(j$type))]
+all_sites$col <- c("#66CC00", "#000066", "#666600", "#003333", "#0066CC", "#FF9900", "#660000")[as.integer(as.factor(all_sites$type))]
 
 # Creation de pop-ups 
-j <- j %>% 
+all_sites <- all_sites %>% 
   mutate(popup_info = paste0("<b> id_cellule</b> ",
                              cell_id,
                              "<br/>",
@@ -44,6 +44,12 @@ j <- j %>%
                              opened_at))
 
 # Observations des especes
-all_sp <- get_obs()
-all_sp_df <- map_df(all_sp[[1]], 1, drop = FALSE)
-dim(all_sp_df)
+# Utilisation de la fonction de Andrew get_obs_df() - modifiée via retrait de la variable "media", qui est une liste et qui beug avec la fonction xtable(xtable) de la fonction renderTable(Shiny)
+
+all_obs <-get_obs_df()
+names(all_obs)
+str(all_obs)
+dim(all_obs)
+
+# Vérification si tous les codes des sites pour les especes observees sont contenus dans la liste de codes de tous les sites existants
+all(unique(all_obs$site_code) %in% unique(all_sites$site_code))
