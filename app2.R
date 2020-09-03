eeea3bfecdc94dbfc0f564d12f2d65494f79c323
Sckende <clrccljuhasz@gmail.com>
2020-09-03 17:29:32 -0400

    # Liste espèce dans le tableau # Reactive () fonctions pour la sélection des données # Typos # Waffle plot pour tous les sites confondus

--- app2.R
+++ app2.R
@@ -13,36 +13,45 @@ if(!exists("all_obs")){source("Manipulations_rcoleo.R")}
 #source("Manipulations_rcoleo.R")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("slate"),
-                fluidRow(
-                  column(4,
-                         selectInput("site",
-                                     "Code du site",
-                                     c("all", sort(unique(all_obs$site_code))))),
-                  column(4,
-                         selectInput("hab",
-                                     "Type d'habitat",
-                                     c("all", sort(unique(all_sites$type))))
-                         )
-                ),
-                fluidRow(
-                  column(6,
-                         plotOutput("waff"))
-                  ,
-                  column(6,
-                         leafletOutput("map"))
-                  
-                  
-                ),
-                fluidRow(
-                column(6,
-                       dataTableOutput("donnees"))
-
-                ),
-                fluidRow(
-                  downloadButton("DL_data", "Télécharger")
-                )
-
+ui <- fluidPage(#theme = shinytheme("slate"),
+  fluidRow(
+    column(4,
+           selectInput("site",
+                       "Code du site",
+                       c("tous", sort(unique(all_obs$site_code))))),
+    column(4,
+           selectInput("hab",
+                       "Type d'échantillonnage",
+                       c("tous", sort(unique(all_sites$type))))
+    )
+  ),
+  fluidRow(
+    column(5,
+           "Description site",
+           fluidRow(
+             column(12,
+                    "Conditions abiotiques",
+                    plotOutput("condAb"))
+           )
+    )
+    ,
+    column(7,
+           leafletOutput("map"))
+    
+    
+  ),
+  fluidRow(
+    column(6,
+           "Répartition type espèce",
+           plotOutput("waff")),
+    column(6,
+           dataTableOutput("donnees"))
+    
+  ),
+  fluidRow(
+    downloadButton("DL_data", "Télécharger")
+  )
+  
 )
 
 # ---------------------------------------------------- #
@@ -50,10 +59,10 @@ ui <- fluidPage(theme = shinytheme("slate"),
 
 server <- function(input, output, session) {
   
-  
+  # -------- # 
   # Map output 
   output$map <- renderLeaflet({
-    if(input$hab == "all"){
+    if(input$hab == "tous"){
       leaflet() %>%
         addTiles() %>% # Affichage du fond de carte
         addCircleMarkers(lng = all_sites$long_site, # Positionnement des sites avec les coordonnées long/lat
@@ -74,31 +83,75 @@ server <- function(input, output, session) {
     }
   })
   
+  # -------- # 
+  output$condAb <- renderPlot(
+    plot(iris$Sepal.Length, type = "l")
+  )
+  
+  
+  # -------- # 
+  # Espèces par site #
+  # ------- #
+  
+  # Waffle plot 
+  
+  xdf <- reactive({
+    if(input$site == "tous"){
+      all_obs %>%
+        count(type)
+    } else {
+      all_obs %>%
+        filter(site_code == input$site) %>%
+        count(type)
+    }
+  })   
+  n_row_2 <- reactive({
+    if(input$site == "tous"){
+      60
+    } else {
+      6
+    }
+  })
   
   output$waff <- renderPlot({
-   xdf <- all_obs %>%  filter(site_code == input$site) %>% count(type)
-    
-    ggplot(xdf, aes(fill=type, values=n)) +
-      geom_waffle(color = "white", size=1.125, n_rows = 6) +
+    ggplot(xdf(), aes(fill = type, values = n)) +
+      geom_waffle(color = "white", size = 1.125, n_rows = n_row_2()) +
       coord_equal() +
-      labs(
-        title = paste("Répartition du type d'espèces sur le site", input$site, sep = " ")
-      ) +
+      #labs(
+      #title = paste("Site", input$site, sep = " ")
+      #) +
       theme_ipsum_rc(grid="") +
       theme_enhance_waffle()
   })
   
-  output$donnees <- renderDataTable(all_obs[all_obs$site_code == input$site,],                                     options = list(pageLength = 20))
   
+  # Tableau de données #  
+  
+  DF_SP <- reactive({
+    if(input$site == "tous"){
+      DF_SP <- all_obs %>% arrange(obs_species.taxa_name) %>%
+        select(obs_species.taxa_name, type) %>% arrange(obs_species.taxa_name)
+    } else {
+      DF_SP <- all_obs %>% filter(site_code == input$site) %>% 
+        select(obs_species.taxa_name, type) %>% arrange(obs_species.taxa_name)
+    }
+  })
+  output$donnees <- renderDataTable(
+    
+    DF_SP()[!duplicated(DF_SP()$obs_species.taxa_name),],
+    options = list(pageLength = 10)
+    
+  )
+  # -------- #  
   output$DL_data <- downloadHandler(
     filename = function() {
-          paste(input$site, paste("_", Sys.Date(), sep = ""), '.csv', sep="")
-        },
-        content = function(file) {
-          write.csv(all_obs[all_obs$site_code == input$site,], file)
-        }
+      paste(input$site, paste("_", Sys.Date(), sep = ""), '.csv', sep="")
+    },
+    content = function(file) {
+      write.csv(DF_SP()[!duplicated(DF_SP()$obs_species.taxa_name),], file)
+    }
   )
-
+  
 }
 
 # Run the application
