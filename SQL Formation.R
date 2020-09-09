c13543f057cbf013b97416f97a1449af773e32b2
Sckende <clrccljuhasz@gmail.com>
2020-09-09 13:25:41 -0400

    # Tests de différentes fonctions

--- SQL Formation.R
+++ SQL Formation.R
@@ -5,3 +5,28 @@ library("sqldf")
 # Exploration of Orange DB
 sqldf("SELECT *
       FROM Orange")
+sqldf("SELECT DISTINCT age
+      FROM Orange")
+
+sqldf("SELECT *
+      FROM Orange
+      WHERE Tree = 1")
+
+sqldf("SELECT *
+      FROM Orange
+      WHERE Tree = 1
+      AND age = 484")
+sqldf("SELECT *
+      FROM Orange
+      WHERE Tree = 1
+      OR Tree = 2")
+
+sqldf("SELECT *
+      FROM Orange
+      ORDER BY age ASC")
+sqldf("SELECT *
+      FROM Orange
+      ORDER BY age")
+# sqldf("SELECT * 
+#       FROM iris
+#       WHERE Species LIKE's%[ar]'")
