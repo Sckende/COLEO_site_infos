c40bbb41463dc7c53d725c25e9c34669dcf22a9d
Sckende <clrccljuhasz@gmail.com>
2020-10-07 15:17:59 -0400

    # script avec subplot() pour les scénarios climatiques

--- Site_explo_version2/app.R
+++ Site_explo_version2/app.R
@@ -14,14 +14,13 @@ library(plotly)
 
 
 if(!exists("all_obs")){source("Manipulations_rcoleo.R")}
-source("waffle_functions.R")
-source("multiplot_functions.R")
-#source("Manipulations_rcoleo.R")
+#source("multiplot_functions.R")
+#source("regions_Ouranos_vs_cellules_COLEO.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(#theme = shinytheme("slate"),
     fluidRow(
-        tags$style(type = "text/css", "#map {height: calc(100vh) !important;}", HTML("#controls {background-color: rgba(0,0,0,0.2); font-color: rgba(1,1,1,0)}")),
+        tags$style(type = "text/css", "#map {height: calc(100vh) !important;}", HTML("#controls {background-color: rgba(255,255,255,0.5); font-color: rgba(1,1,1,0)}")),
         # tags$style(type = "text/css", "#map {height: calc(100vh) !important;}", HTML("#controls {background-color: rgba(0,0,0); opacity: 0.2}") ),
         leafletOutput("map"),
         absolutePanel(id = "controls",
@@ -32,19 +31,32 @@ ui <- fluidPage(#theme = shinytheme("slate"),
                       left = "auto",
                       right = 20,
                       bottom = "auto",
-                      width = 330,
-                      #height = "auto",
-                      height = 665,
+                      #width = 330,
+                      width = "auto",
+                      height = "auto",
+                      #height = 842,
                       selectInput("an",
                                   "Année d'échantillonnage des sites",
                                   unique(sort(all_obs$obs_year))),
                       uiOutput("echan"),
-                      h3("Conditions abiotiques du site sélectionné"),
-                      # plotlyOutput("Temp", height = 230),
-                      # plotOutput("Prec", height = 250)
-                      #plotlyOutput("Prec", height = 250)
-                      plotlyOutput("TempPrec")
-        )
+                      h3("Conditions météorologiques du site"),
+
+                      plotlyOutput("TempPrec")),
+    absolutePanel(id = "controls",
+                  class = "panel panel-default",
+                  fixed = FALSE,
+                  draggable = FALSE,
+                  top = 0,
+                  left = 20,
+                  right = "auto",
+                  bottom = "auto",
+                  #width = 330,
+                  width = "auto",
+                  height = "auto",
+                  #height = 842,
+                  h3("Scénarios changements climatiques pour la région"),
+                  plotlyOutput("scenarios_temp"),
+                  plotlyOutput("scenarios_prec")
     ),
     fluidRow(
         column(6,
@@ -58,7 +70,7 @@ ui <- fluidPage(#theme = shinytheme("slate"),
     fluidRow(
         downloadButton("DL_data", "Télécharger"))
 )
-
+)
 # -------------------------------------------------------- #
 # ------------ When the magic happens -------------------- #
 # -------------------------------------------------------- #
@@ -123,56 +135,252 @@ server <- function(input, output, session) {
             if (is.null(event))
                 return(NULL)
             
-            par(bg = "transparent")
             cell <- unique(obs_an()$cell_id[obs_an()$site_code == event$id])
             temp <- meteoCELLSdf[meteoCELLSdf$cell_id == cell & meteoCELLSdf$indic_meteo == "Temp",]
             temp$Month <- factor(temp$Month, temp$Month)
-            
             prec <- meteoCELLSdf[meteoCELLSdf$cell_id == cell & meteoCELLSdf$indic_meteo == "Prec",]
             prec$Month <- factor(prec$Month, prec$Month)
+
+            # ---- #
             
-            fig1 <- plot_ly(name = "Températures")
-            fig1 <- fig1 %>% add_trace(
-                x = temp$Month,
-                y = temp$Value,
-                type = 'scatter',
-                fill = 'tozeroy',
-                color = "darkorange",
-                hoveron = 'points',
-                marker = list(
-                    color = 'darkorange'
-                ),
-                line = list(
-                    color = 'darkorange'
-                ),
-                text = "Points",
-                hoverinfo = 'x+y'
-            ) 
-            fig1 <- fig1 %>% layout(yaxis = list(title = "Températures moyennes (1979 - 2019)"))
-            #---#
-            fig2 <- plot_ly(
-                x = prec$Month,
-                y = prec$Value,
-                name = "Précipitations",
-                type = "bar",
-                marker = list(color = "rgb(59,122,128")
-            ) 
-            fig2 <- fig2 %>% layout(yaxis = list(title = "Précipitations cumulées (1979 - 2019)"))
-            #---#
-            FIG <- subplot(fig1,
-                           fig2,
-                           nrows = 2,
-                           shareX = TRUE)
-            FIG <- FIG %>% layout(#title = paste0("Profil de températures et de précipitations au site", unique(obs_an()$site_code)),
-                #title = "Profil de températures & précipitations",
-                showlegend = TRUE,
-                legend = list(orientation = "h")) %>% 
-                layout(plot_bgcolor = "rgba(254, 247, 234, 0)") %>% 
-                layout(paper_bgcolor = "rgba(254, 247, 234, 0)") %>% 
-                config(displayModeBar = FALSE)
-            FIG
+            ay <- list(
+                tickfont = list(color = "green"),
+                overlaying = "y",
+                side = "right",
+                title = "Précipitations cumulées (mm)",
+                showgrid = F)
+            
+            over <- plot_ly()
+            over <- over %>% add_lines(x = temp$Month,
+                                       y = temp$Value,
+                                       type = "scatter",
+                                       mode = "lines+markers",
+                                       marker = list(symbol = "circle",
+                                                     color = 'darkorange'),
+                                       line = list(color = "darkorange"),
+                                       fill = "tozeroy",
+                                       fillcolor = "rgba(255,126,0,0.4)",
+                                       name = "Températures")
+            over <- over %>% add_trace(x = prec$Month,
+                                       y = prec$Value,
+                                       name = "Précipitations",
+                                       type = "bar",
+                                       opacity = 0.5,
+                                       orientation = "v",
+                                       marker = list(color = "green"),
+                                       yaxis = "y2")
+            over <- over %>%  layout(title = event$id,
+                                     yaxis = list(title = "Températures moyennes (C)",
+                                                  tickfont = list(color = "darkorange"),
+                                                  showgrid = F),
+                                     yaxis2 = ay,
+                                     xaxis = list(title = "Mois",
+                                                  showgrid = F),
+                                     showlegend = TRUE,
+                                     legend = list(orientation = 'v', # show entries horizontally
+                                                   xanchor = "center", # use center of legend as anchor
+                                                   x = 0.5,
+                                                   y = -1), #put legend in center of x-axis
+                                     margin = list(I = 80,
+                                                   r = 80,
+                                                   t = 100,
+                                                   b = 100,
+                                                   autoexpand = TRUE)) %>%
+                config(displayModeBar = FALSE) %>% 
+                layout(plot_bgcolor = "rgba(254, 247, 234, 0)") %>%
+                layout(paper_bgcolor = "rgba(254, 247, 234, 0)")
+            over
         })
         
+        output$scenarios_temp <- renderPlotly({
+            
+            
+            if (is.null(event))
+                return(NULL)
+            
+            #---#
+            reg <- unique(obs_an()$Region[obs_an()$site_code == event$id])
+            scenar <- scenario_meteo[scenario_meteo$Region == reg,]
+            
+            #---#
+            figTemp <- plot_ly(x = scenar$Annee[scenar$param_met == "temp"],
+                               y = scenar$Obs[scenar$param_met == "temp"],
+                               type = "scatter",
+                               mode = "lines",
+                               line = list(color = 'darkorange'),
+                               name = "hist_temp",
+                               showlegend = TRUE) %>% 
+                add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                          y = scenar$Hist.Max[scenar$param_met == "temp"],
+                          type = "scatter",
+                          mode = "lines",
+                          fill = "tonexty",
+                          fillcolor='rgba(153,102,0,0.2)',
+                          line = list(color = 'transparent'),
+                          name = "hist_max",
+                          showlegend = FALSE) %>% 
+                add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                          y = scenar$Hist.Min[scenar$param_met == "temp"],
+                          type = "scatter",
+                          mode = "lines",
+                          fill = "tonexty",
+                          fillcolor='rgba(153,102,0,0.2)',
+                          line = list(color = 'transparent'),
+                          name = "hist_min",
+                          showlegend = FALSE)
+            figTemp <- figTemp %>% add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                                             y = scenar$rcp45.Avg[scenar$param_met == "temp"],
+                                             type = "scatter",
+                                             mode = "lines",
+                                             line = list(color = 'rgba(153,102,0,1)'),
+                                             name = "emission_moderees",
+                                             showlegend = TRUE) %>% 
+                add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                          y = scenar$rcp45.Max[scenar$param_met == "temp"],
+                          type = "scatter",
+                          mode = "lines",
+                          fill = "tonexty",
+                          fillcolor='rgba(153,102,0,0.2)',
+                          line = list(color = 'transparent'),
+                          name = "emission_moderee_max",
+                          showlegend = FALSE) %>% 
+                add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                          y = scenar$rcp45.Min[scenar$param_met == "temp"],
+                          type = "scatter",
+                          mode = "lines",
+                          fill = "tonexty",
+                          fillcolor='rgba(153,102,0,0.2)',
+                          line = list(color = 'transparent'),
+                          name = "emission_moderee_min",
+                          showlegend = FALSE)
+            figTemp <- figTemp %>% add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                                             y = scenar$rcp85.Avg[scenar$param_met == "temp"],
+                                             type = "scatter",
+                                             mode = "lines",
+                                             line = list(color = "rgba(255,51,51,1)"),
+                                             name = "emission_fortes",
+                                             showlegend = TRUE) %>% 
+                add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                          y = scenar$rcp85.Max[scenar$param_met == "temp"],
+                          type = "scatter",
+                          mode = "lines",
+                          fill = "tonexty",
+                          fillcolor='rgba(255,51,51,0.2)',
+                          line = list(color = 'transparent'),
+                          name = "emission_forte_max",
+                          showlegend = FALSE) %>% 
+                add_trace(x = scenar$Annee[scenar$param_met == "temp"],
+                          y = scenar$rcp85.Min[scenar$param_met == "temp"],
+                          type = "scatter",
+                          mode = "lines",
+                          fill = "tonexty",
+                          fillcolor= "rgba(255,51,51,0.2)",
+                          line = list(color = 'transparent'),
+                          name = "emission_forte_min",
+                          showlegend = FALSE)
+            figTemp <- figTemp %>% 
+                layout(title = reg,
+                       yaxis = list(title = "Températures moyennes (C)",
+                                    #tickfont = list(color = "darkorange"),
+                                    showgrid = F),
+                       xaxis = list(title = "Année",
+                                    showgrid = F),
+                       showlegend = TRUE,
+                       legend = list(orientation = 'h', # show entries horizontally
+                                     xanchor = "center", # use center of legend as anchor
+                                     x = 0.5,
+                                     y = -0.5), #put legend in center of x-axis
+                       margin = list(I = 80,
+                                     r = 80,
+                                     t = 100,
+                                     b = 100,
+                                     autoexpand = TRUE)) %>%
+                layout(plot_bgcolor = "rgba(254, 247, 234, 0)") %>%
+                layout(paper_bgcolor = "rgba(254, 247, 234, 0)") %>%
+                config(displayModeBar = FALSE)
+            })
+
+        #---#
+        output$scenarios_prec <- renderPlotly({
+            
+            
+            if (is.null(event))
+                return(NULL)
+            #---#
+            reg <- unique(obs_an()$Region[obs_an()$site_code == event$id])
+            scenar <- scenario_meteo[scenario_meteo$Region == reg,]
+            #---#
+        figPrec <- plot_ly(x = scenar$Annee[scenar$param_met == "prec"],
+                           y = scenar$Obs[scenar$param_met == "prec"],
+                           type = "scatter",
+                           mode = "lines",
+                           #line = list(color = 'rgb(59,122,128'),
+                           line = list(color = "green"),
+                           name = "hist_prec",
+                           showlegend = TRUE)
+        figPrec <- figPrec %>% add_trace(x = scenar$Annee[scenar$param_met == "prec"],
+                                         y = scenar$rcp45.Avg[scenar$param_met == "prec"],
+                                         type = "scatter",
+                                         mode = "lines",
+                                         line = list(color = 'rgba(153,102,0,1)'),
+                                         name = "emission_moderees",
+                                         showlegend = TRUE)
+        figPrec <- figPrec %>% add_trace(x = scenar$Annee[scenar$param_met == "prec"],
+                                         y = scenar$rcp85.Avg[scenar$param_met == "prec"],
+                                         type = "scatter",
+                                         mode = "lines",
+                                         line = list(color = "rgba(255,51,51,1)"),
+                                         name = "emission_fortes",
+                                         showlegend = TRUE)
+        figPrec <- figPrec %>% 
+            layout(yaxis = list(title = "Précipitations cumulées",
+                                showgrid = F),
+                   xaxis = list(title = "Année",
+                                showgrid = F),
+                   showlegend = TRUE,
+                   legend = list(orientation = 'h', # show entries horizontally
+                                 xanchor = "center", # use center of legend as anchor
+                                 x = 0.5,
+                                 y = -0.5), #put legend in center of x-axis
+                   margin = list(I = 80,
+                                 r = 80,
+                                 t = 20,
+                                 b = 100,
+                                 autoexpand = TRUE)) %>%
+            layout(plot_bgcolor = "rgba(254, 247, 234, 0)") %>%
+            layout(paper_bgcolor = "rgba(254, 247, 234, 0)") %>%
+            config(displayModeBar = FALSE)
+        #---#
+        # scen_fig <- subplot(figTemp,
+        #                     figPrec,
+        #                     nrows = 2,
+        #                     shareX = TRUE)
+        # scen_fig <- scen_fig %>% 
+        #     layout(title = reg,
+        #            yaxis = list(title = "Températures moyennes (C)",
+        #                         tickfont = list(color = "darkorange"),
+        #                         showgrid = F),
+        #            yaxis2 = list(title = "Précipitations cumulées",
+        #                          tickfont = list(color = "green"),
+        #                          showgrid = F),
+        #            xaxis = list(title = "Mois",
+        #                         showgrid = F),
+        #            showlegend = TRUE,
+        #            legend = list(orientation = 'v', # show entries horizontally
+        #                          xanchor = "center", # use center of legend as anchor
+        #                          x = 0.5,
+        #                          y = -1), #put legend in center of x-axis
+        #            margin = list(I = 80,
+        #                          r = 80,
+        #                          t = 100,
+        #                          b = 100,
+        #                          autoexpand = TRUE)) %>% 
+        #     layout(plot_bgcolor = "rgba(254, 247, 234, 0)") %>% 
+        #     layout(paper_bgcolor = "rgba(254, 247, 234, 0)") %>% 
+        #     config(displayModeBar = FALSE)
+        # scen_fig
+
+    })
         # Obtention de la liste des espèces observées lors de l'échantillonnage TOUTES CAMPAGNES CONFONDUES
         #----------------------------------------------------------------------
         
